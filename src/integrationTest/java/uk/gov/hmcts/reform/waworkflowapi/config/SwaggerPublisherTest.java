049922ecf5c86b3fe7a3dcce1d74a9695e5709c9
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2020-08-14 12:23:45 +0100

    updated swagger.yml

--- src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/config/SwaggerPublisherTest.java
@@ -1,17 +1,12 @@
 package uk.gov.hmcts.reform.waworkflowapi.config;
 
-import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockFilterConfig;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -19,32 +14,18 @@ import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
 
 /**
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
-@SpringJUnitWebConfig
-@SpringBootTest
+@WebMvcTest
+@ContextConfiguration(classes = SwaggerConfiguration.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("integration")
 class SwaggerPublisherTest {
 
     @Autowired
     private MockMvc mvc;
-
-    @Autowired
-    private WebApplicationContext wac;
-
-    @BeforeEach
-    void setUp() {
-        WebRequestTrackingFilter filter;
-        filter = new WebRequestTrackingFilter();
-        filter.init(new MockFilterConfig());
-        mvc = webAppContextSetup(wac).addFilters(filter).build();
-    }
 
     @DisplayName("Generate swagger documentation")
     @Test
