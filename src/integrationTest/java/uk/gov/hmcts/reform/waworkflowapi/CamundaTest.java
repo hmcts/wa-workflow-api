253e790f3914aa7bf54cb919da6f46cd909facad
Lewis Williams <lewis.williams@hmcts.net>
2020-08-27 13:52:30 +0100

    moved camunda test

--- src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/CamundaTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/CamundaTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.waworkflowapi.config;
+package uk.gov.hmcts.reform.waworkflowapi;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
