5d9fb1ce7f233ed796c3967f950ef8f1134e0491
Lewis Williams <lewis.williams@hmcts.net>
2020-08-27 12:44:11 +0100

    added version test

--- src/test/java/uk/gov/hmcts/reform/rsecheck/CamundaTest.java
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/CamundaTest.java
@@ -14,10 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CamundaTest {
 
     public static final String URL = "http://camunda-api-aat.service.core-compute-aat.internal";
+
     @Test
     public void camundaHealthTest() throws IOException {
         HttpClient client = new HttpClient();
-        HttpMethod method = new GetMethod(URL+"/health");
+        HttpMethod method = new GetMethod(URL + "/health");
         client.executeMethod(method);
         assertEquals(method.getStatusCode(),HttpStatus.SC_OK,"");
         method.releaseConnection();
@@ -26,7 +27,7 @@ public class CamundaTest {
     @Test
     public void camundaVersionTest() throws IOException {
         HttpClient client = new HttpClient();
-        HttpMethod method = new GetMethod(URL+"/engine-rest/version");
+        HttpMethod method = new GetMethod(URL + "/engine-rest/version");
         client.executeMethod(method);
         assertEquals(method.getResponseBodyAsString(),"\"version\": \"7.13.3-ee\"","");
         method.releaseConnection();
