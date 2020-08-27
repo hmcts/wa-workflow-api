89cfaf5c17bff76763d8fc075e76b6d628f28ce6
Lewis Williams <lewis.williams@hmcts.net>
2020-08-27 13:45:15 +0100

    added process definition test

--- src/test/java/uk/gov/hmcts/reform/rsecheck/CamundaTest.java
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/CamundaTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,10 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CamundaTest {
 
     public static final String URL = "http://camunda-api-aat.service.core-compute-aat.internal";
+    HttpClient client = new HttpClient();
 
     @Test
     public void camundaHealthTest() throws IOException {
-        HttpClient client = new HttpClient();
         HttpMethod method = new GetMethod(URL + "/health");
         client.executeMethod(method);
         assertEquals(method.getStatusCode(),HttpStatus.SC_OK,"");
@@ -26,10 +27,18 @@ public class CamundaTest {
 
     @Test
     public void camundaVersionTest() throws IOException {
-        HttpClient client = new HttpClient();
         HttpMethod method = new GetMethod(URL + "/engine-rest/version");
         client.executeMethod(method);
         assertEquals(method.getResponseBodyAsString(),"{\"version\":\"7.13.3-ee\"}","");
+        assertEquals(method.getStatusCode(),HttpStatus.SC_OK,"");
+        method.releaseConnection();
+    }
+
+    @Test
+    public void camundaProcessDefinitionTest() throws IOException {
+        HttpMethod method = new GetMethod(URL + "/engine-rest/decision-definition");
+        client.executeMethod(method);
+        assertEquals(method.getStatusCode(),HttpStatus.SC_OK,"");
         method.releaseConnection();
     }
 }
