3fb8b0f0277b4aaeef1152f6268e6d53dfef0e26
Lewis Williams <lewis.williams@hmcts.net>
2020-09-03 16:05:19 +0100

    updated for checkstyle

--- src/test/java/uk/gov/hmcts/reform/waworkflowapi/external/taskservice/GetTaskServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/waworkflowapi/external/taskservice/GetTaskServiceTest.java
@@ -1,31 +1,31 @@
 package uk.gov.hmcts.reform.waworkflowapi.external.taskservice;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import uk.gov.hmcts.reform.waworkflowapi.camudaRestapiWrapper.GetCamundaTaskService;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-
 @SuppressWarnings("PMD.JUnitAssertionsShouldIncludeMessage")
 class GetTaskServiceTest {
 
-    @Mock
-    private GetCamundaTaskService taskService;
+  @Mock
+  private GetCamundaTaskService taskService;
 
 
-    @BeforeEach
-    void setUp() {
-        taskService = mock(GetCamundaTaskService.class);
-        Mockito.when(taskService.getTaskByID("SomeId")).thenReturn("test object string");
+  @BeforeEach
+  void setUp() {
+    taskService = mock(GetCamundaTaskService.class);
+    Mockito.when(taskService.getTaskByID("SomeId"))
+        .thenReturn("test object string");
+  }
 
-    }
-
-    @Test
-    void getsATaskBasedOnId() {
+  @Test
+  void getsATaskBasedOnId() {
     String response = taskService.getTaskByID("SomeId");
-    assertEquals(response,"test object string");
-    }
+    assertEquals(response, "test object string");
+  }
 }
