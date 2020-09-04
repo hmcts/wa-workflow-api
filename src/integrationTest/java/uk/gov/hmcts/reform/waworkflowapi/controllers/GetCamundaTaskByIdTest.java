a479513242ff2a0d71b581c85d130e07e58cae0a
Lewis Williams <lewis.williams@hmcts.net>
2020-09-04 11:05:02 +0100

    fixed test order

--- src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/controllers/GetCamundaTaskByIdTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/waworkflowapi/controllers/GetCamundaTaskByIdTest.java
@@ -2,13 +2,14 @@ package uk.gov.hmcts.reform.waworkflowapi.controllers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
+
 import org.junit.jupiter.api.Test;
 
 
