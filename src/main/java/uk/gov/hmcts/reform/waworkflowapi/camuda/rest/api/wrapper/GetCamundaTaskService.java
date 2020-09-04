e3e0bee041484d6f545b7f84c4fff5c874d7b53a
Lewis Williams <lewis.williams@hmcts.net>
2020-09-04 15:12:51 +0100

    fixed test order

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/camuda/rest/api/wrapper/GetCamundaTaskService.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/camuda/rest/api/wrapper/GetCamundaTaskService.java
@@ -4,14 +4,18 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 
 @Service
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public class GetCamundaTaskService {
 
+    private static final Logger logger = LoggerFactory.getLogger(GetCamundaTaskService.class);
 
     private static final String URL = "http://camunda-api-aat.service.core-compute-aat.internal/engine-rest";
 
@@ -26,7 +30,7 @@ public class GetCamundaTaskService {
                 return method.getResponseBodyAsString();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(MessageFormat.format("Exception: {0}", e));
         } finally {
             method.releaseConnection();
         }
