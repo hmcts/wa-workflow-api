3549b9a7be86dba38eaec13a5a36fb8de23d5aa2
Lewis Williams <lewis.williams@hmcts.net>
2020-09-04 13:02:58 +0100

    fixed test order

--- src/test/java/uk/gov/hmcts/reform/waworkflowapi/external/taskservice/GetTaskServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/waworkflowapi/external/taskservice/GetTaskServiceTest.java
@@ -13,22 +13,22 @@ import static org.mockito.Mockito.mock;
 @SuppressWarnings("PMD.JUnitAssertionsShouldIncludeMessage")
 class GetTaskServiceTest {
 
-  @Mock
-  private GetCamundaTaskService taskService;
+    @Mock
+    private GetCamundaTaskService taskService;
 
 
-  @BeforeEach
-  void setUp() {
-    taskService = mock(GetCamundaTaskService.class);
-    Mockito.when(taskService.getTaskByID("SomeId"))
-        .thenReturn("test object string");
-  }
+    @BeforeEach
+    void setUp() {
+        taskService = mock(GetCamundaTaskService.class);
+         Mockito.when(taskService.getTaskByID("SomeId"))
+            .thenReturn("test object string");
+    }
 
-  @Test
-  void getsATaskBasedOnId() {
-    String response = taskService.getTaskByID("SomeId");
-    assertEquals("test object string", response);
-  }
+    @Test
+    void getsATaskBasedOnId() {
+        String response = taskService.getTaskByID("SomeId");
+        assertEquals("test object string", response);
+    }
 
 
     @Test
