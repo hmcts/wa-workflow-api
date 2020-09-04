de48b64ea64ae5b318443d95b3dedb528a5b1a6e
Lewis Williams <lewis.williams@hmcts.net>
2020-09-04 11:57:39 +0100

    fixed test order

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/camudaRestapiWrapper/GetCamundaTaskService.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/camudaRestapiWrapper/GetCamundaTaskService.java
@@ -11,17 +11,13 @@ import java.io.IOException;
 @Service
 public class GetCamundaTaskService {
 
-//    private static final String PROXY_HOST = "proxyout.reform.hmcts.net";
-//    private static final int PROXY_PORT = 8080;
-    private static final String URL = "http://camunda-api-aat.service.core-compute-aat.internal/engine-rest";
-    public String getTaskByID(String id)  {
-        HttpClient client = new HttpClient();
-        HttpMethod method = new GetMethod(URL+"/task/"+id);
-//        HostConfiguration config = client.getHostConfiguration();
-//        config.setProxy(PROXY_HOST, PROXY_PORT);
 
-//        AuthScope authScope = new AuthScope(PROXY_HOST, PROXY_PORT);
-//        client.getState().setProxyCredentials(authScope, null);
+    private static final String URL = "http://camunda-api-aat.service.core-compute-aat.internal/engine-rest";
+
+    public String getTaskByID(String id) {
+        HttpClient client = new HttpClient();
+        HttpMethod method = new GetMethod(URL + "/task/" + id);
+
         try {
             client.executeMethod(method);
 
--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/models/Task.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/models/Task.java
@@ -2,29 +2,30 @@ package uk.gov.hmcts.reform.waworkflowapi.models;
 
 public class Task {
 
-        private String id;
-        private String name;
-        private String assignee;
-        private String created;
-        private String due;
-        private String followUp;
-        private String delegationState;
-        private String description;
-        private String executionId;
-        private String owner;
-        private String parentTaskId;
-        private int priority;
-        private String processDefinitionId;
-        private String processInstanceId;
-        private String taskDefinitionKey;
-        private String caseExecutionId;
-        private String caseInstanceId;
-        private String caseDefinitionId;
-        private boolean suspended;
-        private String formKey;
-        private String tenantId;
+    private String id;
+    private String name;
+    private String assignee;
+    private String created;
+    private String due;
+    private String followUp;
+    private String delegationState;
+    private String description;
+    private String executionId;
+    private String owner;
+    private String parentTaskId;
+    private int priority;
+    private String processDefinitionId;
+    private String processInstanceId;
+    private String taskDefinitionKey;
+    private String caseExecutionId;
+    private String caseInstanceId;
+    private String caseDefinitionId;
+    private boolean suspended;
+    private String formKey;
+    private String tenantId;
 
-        public Task(){}
+    public Task() {
+    }
 
     public String getId() {
         return id;
