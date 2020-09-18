bba73e3c8fa9c5bcd6aeae9ade8ea82fdcb0f395
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-18 12:18:50 +0100

    using app_settings to define secret and vault id with resource group instead of uri

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,16 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_key_vault_secret" "s2s_key" {
-  name      = "microservicekey-wa-workflow-api"
-  vault_uri = "https://s2s-${var.env}.vault.azure.net/"
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  key_vault_id = data.azurerm_key_vault.s2s_key_vault.id
+  name         = "microservicekey-wa-workflow-api"
+}
+
+app_settings = {
+  S2S_SECRET_WORKFLOW_API = "${data.azurerm_key_vault_secret.s2s_secret.value}"
 }
