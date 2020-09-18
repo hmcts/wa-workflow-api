ccf5772850f9293776fff634aaa6663c20fcb17f
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-18 11:46:29 +0100

    Attempt to retrieve s2s secret from s2s keyvault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   features {}
 }
+
+data "azurerm_key_vault_secret" "s2s_key" {
+  name      = "microservicekey-wa-workflow-api"
+  vault_uri = "https://s2s-${var.env}.vault.azure.net/"
+}
