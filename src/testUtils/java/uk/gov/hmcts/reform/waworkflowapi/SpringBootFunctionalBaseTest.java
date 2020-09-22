a7894e705d5d4d21d6815945c8ef5d985b8c313b
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-22 14:51:48 +0100

    Fixes Serenity test reports as functional tests weren't using the SpringIntegrationSerenityRunner

--- src/testUtils/java/uk/gov/hmcts/reform/waworkflowapi/SpringBootFunctionalBaseTest.java
+++ src/testUtils/java/uk/gov/hmcts/reform/waworkflowapi/SpringBootFunctionalBaseTest.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.waworkflowapi;
 
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @SpringBootTest
 @ActiveProfiles("functional")
 public abstract class SpringBootFunctionalBaseTest {
