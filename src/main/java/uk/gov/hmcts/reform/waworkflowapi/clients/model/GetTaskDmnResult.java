4788b4e56291615b88c35852e5e4c1e82d400c50
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-12-17 09:21:42 +0000

    Increase mutation test coverage

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/model/GetTaskDmnResult.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/model/GetTaskDmnResult.java
@@ -1,7 +1,12 @@
 package uk.gov.hmcts.reform.waworkflowapi.clients.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.util.Objects;
 
+@EqualsAndHashCode
+@ToString
 public class GetTaskDmnResult {
     private DmnValue<String> taskId;
     private DmnValue<String> group;
@@ -32,35 +37,5 @@ public class GetTaskDmnResult {
 
     public DmnValue<String> getName() {
         return name;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        GetTaskDmnResult that = (GetTaskDmnResult) object;
-        return Objects.equals(taskId, that.taskId)
-               && Objects.equals(group, that.group)
-               && Objects.equals(workingDaysAllowed, that.workingDaysAllowed)
-               && Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(taskId);
-    }
-
-    @Override
-    public String toString() {
-        return "GetTaskDmnResult{"
-               + "taskId=" + taskId
-               + ", group=" + group
-               + ", workingDaysAllowed=" + workingDaysAllowed
-               + ", name=" + name
-               + '}';
     }
 }
