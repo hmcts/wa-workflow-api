b08566dc983f5935d5f0ce4b65da61da39bd89ec
david.crespo <david.crespo@hmcts.net>
2021-03-10 09:20:16 +0000

    add error handling

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/idempotency/IdempotencyTaskWorkerHandler.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/idempotency/IdempotencyTaskWorkerHandler.java
@@ -6,6 +6,7 @@ import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskService;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotencykey.IdempotentId;
+import uk.gov.hmcts.reform.waworkflowapi.exceptions.IdempotencyTaskWorkerException;
 
 import java.util.Optional;
 
@@ -16,6 +17,10 @@ import static uk.gov.hmcts.reform.waworkflowapi.clients.service.idempotency.Idem
 @Slf4j
 public class IdempotencyTaskWorkerHandler {
 
+    public static final int NUMBER_OF_RETRIES = 3;
+    public static final int NO_MORE_RETRIES = 1;
+    public static final int INCIDENT_SIGNAL = 0;
+
     private final IdempotencyTaskService idempotencyTaskService;
 
     public IdempotencyTaskWorkerHandler(
@@ -25,10 +30,35 @@ public class IdempotencyTaskWorkerHandler {
 
     public void checkIdempotency(ExternalTask externalTask, ExternalTaskService externalTaskService) {
         log.info("checking idempotency...");
-        Optional<IdempotentId> idempotentId = getIdempotentId(externalTask);
-        idempotentId.ifPresentOrElse(
-            id -> idempotencyTaskService.handleIdempotentIdProvidedScenario(externalTask, externalTaskService, id),
-            () -> completeTask(externalTask, externalTaskService)
+        try {
+            Optional<IdempotentId> idempotentId = getIdempotentId(externalTask);
+            idempotentId.ifPresentOrElse(
+                id -> idempotencyTaskService.handleIdempotentIdProvidedScenario(externalTask, externalTaskService, id),
+                () -> completeTask(externalTask, externalTaskService)
+            );
+        } catch (Exception e) {
+            handleError(externalTask, externalTaskService);
+        }
+    }
+
+    private void handleError(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        if (externalTask.getRetries() == null) {
+            setFailure(externalTask, externalTaskService, NUMBER_OF_RETRIES);
+        } else if (externalTask.getRetries() > NO_MORE_RETRIES) {
+            setFailure(externalTask, externalTaskService, externalTask.getRetries() - 1);
+        } else {
+            setFailure(externalTask, externalTaskService, INCIDENT_SIGNAL);
+            throw new IdempotencyTaskWorkerException("*** ERROR with idempotency worker *** ");
+        }
+    }
+
+    private void setFailure(ExternalTask externalTask, ExternalTaskService externalTaskService, int retries) {
+        externalTaskService.handleFailure(
+            externalTask.getId(),
+            "david error message",
+            "david error details",
+            retries,
+            1000
         );
     }
 
