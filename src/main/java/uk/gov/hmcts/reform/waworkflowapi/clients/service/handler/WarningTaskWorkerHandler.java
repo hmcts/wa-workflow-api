e4a9f023f14220c45b10c8a3ed56371a1a1f3065
david.crespo <david.crespo@hmcts.net>
2021-04-28 17:43:17 +0100

    add logs

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/handler/WarningTaskWorkerHandler.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/clients/service/handler/WarningTaskWorkerHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.waworkflowapi.clients.service.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.client.task.ExternalTask;
 import org.camunda.bpm.client.task.ExternalTaskService;
 import org.springframework.stereotype.Component;
@@ -7,10 +8,14 @@ import org.springframework.stereotype.Component;
 import static java.util.Collections.singletonMap;
 
 @Component
+@Slf4j
 public class WarningTaskWorkerHandler {
 
     public void setTaskWarningFlag(ExternalTask externalTask, ExternalTaskService externalTaskService) {
-        externalTaskService.complete(externalTask, null, singletonMap("hasWarnings", true));
+        var caseId = externalTask.getVariable("caseId");
+        log.info("Setting warning for task(id={}, caseId={})...", externalTask.getId(), caseId);
+        externalTaskService.complete(externalTask, singletonMap("hasWarnings", true));
+        log.info("Warning completed successfully for task(id={}, caseId={})", externalTask.getId(), caseId);
     }
 }
 
