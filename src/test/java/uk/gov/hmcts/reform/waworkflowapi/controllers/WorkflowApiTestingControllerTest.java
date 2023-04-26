12fb0516e0be469de4655bf0e24237bdeb3792cd
serdaroguzhanakin <akinserdaroguzhan@gmail.com>
2023-04-26 14:50:04 +0100

    test coverage increased

--- src/test/java/uk/gov/hmcts/reform/waworkflowapi/controllers/WorkflowApiTestingControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/waworkflowapi/controllers/WorkflowApiTestingControllerTest.java
@@ -11,11 +11,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.waworkflowapi.clients.model.idempotencykey.IdempotencyKeys;
 import uk.gov.hmcts.reform.waworkflowapi.clients.service.idempotency.IdempotencyTaskService;
+import uk.gov.hmcts.reform.waworkflowapi.exceptions.ResourceNotFoundException;
+import uk.gov.hmcts.reform.waworkflowapi.exceptions.UnAuthorizedException;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,7 +40,7 @@ class WorkflowApiTestingControllerTest {
     }
 
     @Test
-    void post_messages_should_delegate_to_eventMessageReceiverService() {
+    void should_return_idempotency_keys_with_response_code_200() {
         IdempotencyKeys idempotencyKeys = new IdempotencyKeys(
             IDEMPOTENCY_KEY, JURISDICTION, PROCESS_ID, DATE, DATE
         );
@@ -49,6 +52,30 @@ class WorkflowApiTestingControllerTest {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(idempotencyKeys);
+
+    }
+
+    @Test
+    void should_throw_unauthorized_with_response_code_401_when_env_prod() {
+        ReflectionTestUtils.setField(controller, "environment", "prod");
+
+        assertThatThrownBy(() -> controller.checkIdempotencyKey(IDEMPOTENCY_KEY, JURISDICTION))
+            .isInstanceOf(UnAuthorizedException.class)
+            .hasMessage("Request Unauthorized for this environment");
+
+    }
+
+    @Test
+    void should_throw_unauthorized_with_response_code_404_when_record_not_found() {
+
+        doReturn(Optional.empty())
+            .when(idempotencyTaskService)
+            .findByIdempotencyKeyAndTenantId(IDEMPOTENCY_KEY, JURISDICTION);
+
+        assertThatThrownBy(() -> controller.checkIdempotencyKey(IDEMPOTENCY_KEY, JURISDICTION))
+            .isInstanceOf(ResourceNotFoundException.class)
+            .hasMessage(
+                String.format("Resource not found idempotencyKey:%s jurisdiction:%s", IDEMPOTENCY_KEY, JURISDICTION));
 
     }
 
