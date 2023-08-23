7fc4eba3579b3392912b301334c0483e5eae5213
dharmendra kumar <dharmendra.kumar@hmcts.net>
2023-08-23 10:17:53 +0100

    revert unnecesary changes

--- bin/pull-latest-dmn-files.sh
+++ bin/pull-latest-dmn-files.sh
@@ -3,15 +3,15 @@
 branchName=$1
 
 #Checkout specific branch pf  camunda bpmn definition
-git clone https://github.com/hmcts/et-wa-task-configuration.git
-cd et-wa-task-configuration
+git clone https://github.com/hmcts/wa-task-configuration-template.git
+cd wa-task-configuration-template
 
-echo "Switch to ${branchName} branch on et-wa-task-configuration"
+echo "Switch to ${branchName} branch on wa-task-configuration-template"
 git checkout ${branchName}
 cd ..
 
 #Copy camunda folder which contains dmn files
-cp -r ./et-wa-task-configuration/src/main/resources .
-rm -rf ./et-wa-task-configuration
+cp -r ./wa-task-configuration-template/src/main/resources .
+rm -rf ./wa-task-configuration-template
 
 ./bin/import-dmn-diagram.sh . wa wa
