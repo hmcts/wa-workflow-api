248113db8175330b4d2e8d52fb8ee2daaed6ffa6
raghera <raghera@hotmail.com>
2024-02-09 15:31:10 +0000

    Update Application.java
    checkstyle

--- src/main/java/uk/gov/hmcts/reform/waworkflowapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/waworkflowapi/Application.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 @EnableFeignClients(basePackages = {
-        "uk.gov.hmcts.reform.authorisation",
-        "uk.gov.hmcts.reform.waworkflowapi",
-    })
+    "uk.gov.hmcts.reform.authorisation",
+    "uk.gov.hmcts.reform.waworkflowapi",
+})
 public class Application {
 
     public static void main(final String[] args) {
