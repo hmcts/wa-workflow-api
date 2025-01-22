549f1140b1d5db3cc9e7067f37586a4f8c18856e
Antony Leons <antony@leons.dev>
2025-01-22 15:29:10 +0000

    readd vars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -22,6 +22,15 @@ variable "common_tags" {
   type = map(string)
 }
 
+
+variable "postgresql_database_name" {
+  default = "wa_workflow_api"
+}
+
+variable "postgresql_user" {
+  default = "wa_wa"
+}
+
 variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
