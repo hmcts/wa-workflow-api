697c0edf2c4543be93953d4b557fbde278be74e0
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2026-02-16 15:26:42 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -55,7 +55,7 @@ module "wa_workflow_api_database_flex" {
   email_address_key          = var.email_address_key
   email_address_key_vault_id = data.azurerm_key_vault.wa_key_vault.id
 
-  auto_grow_enabled          = true
+  auto_grow_enabled = true
 
   pgsql_databases = [
     {
